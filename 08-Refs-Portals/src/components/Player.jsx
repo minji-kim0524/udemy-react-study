0a2c7533b97a60f9b25b882768bc367e801c3fc1
MINJI <always0524@naver.com>
2025-09-09 22:54:35 +0900

    feat. 참조&포탈 실습 - useState를 이용한 입력값 반영

--- 08-Refs-Portals/src/components/Player.jsx
+++ 08-Refs-Portals/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    // 타이핑 수정까지 실시간으로 반영하는 것을 방지하기위해
+    // setSubmitted 를 false 로 설정
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
