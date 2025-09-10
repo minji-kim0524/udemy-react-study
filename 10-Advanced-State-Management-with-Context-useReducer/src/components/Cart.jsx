56189f4ef1ad7b46579ef00fe952b09d399c3317
MINJI <always0524@naver.com>
2025-09-10 20:30:41 +0900

    refactor. 컨텍스트 API - 컨텍스트를 활용하여 코드정리

--- 10-Advanced-State-Management-with-Context-useReducer/src/components/Cart.jsx
+++ 10-Advanced-State-Management-with-Context-useReducer/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const cartCtx = useContext(CartContext); // useContext는 컨테스트 컴포넌트를 받음
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext); // useContext는 컨테스트 컴포넌트를 받음
 
   return (
     <CartContext.Consumer>
@@ -27,15 +27,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                         <span> ({formattedPrice})</span>
                       </div>
                       <div className="cart-item-actions">
-                        <button
-                          onClick={() => onUpdateItemQuantity(item.id, -1)}
-                        >
+                        <button onClick={() => updateItemQuantity(item.id, -1)}>
                           -
                         </button>
                         <span>{item.quantity}</span>
-                        <button
-                          onClick={() => onUpdateItemQuantity(item.id, 1)}
-                        >
+                        <button onClick={() => updateItemQuantity(item.id, 1)}>
                           +
                         </button>
                       </div>
