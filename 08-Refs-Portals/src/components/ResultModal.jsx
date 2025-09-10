5b4290f74fd8896f567fecda9ff85c0a8a0df9d9
MINJI <always0524@naver.com>
2025-09-10 11:13:20 +0900

    feat. 참조&포탈 실습 - createPortal 적용

--- 08-Refs-Portals/src/components/ResultModal.jsx
+++ 08-Refs-Portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
-import { useImperativeHandle, useRef } from "react";
+import { useImperativeHandle, useRef } from "react"; // 모든 환경에서 사용가능한 함수, 기능을 불러옴(리액트 네이티브)
+import { createPortal } from "react-dom"; // 리액트가 DOM과 상효작용하게 함
 
 export default function ResultModal({
   ref,
@@ -10,6 +11,7 @@ export default function ResultModal({
 
   const userLost = remainingTime <= 0;
   const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   // 이 컴포넌트 외부에서 프로퍼티와 메서드를 통해 접근할 수 있도록 해주는 리액트 훅
   // 첫 번째 인수: ref 객체
@@ -22,9 +24,10 @@ export default function ResultModal({
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
@@ -35,6 +38,7 @@ export default function ResultModal({
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 }
