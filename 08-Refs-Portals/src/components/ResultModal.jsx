4bce810289ac7684ce452a67fd404cf3829f6ff8
MINJI <always0524@naver.com>
2025-09-10 10:07:58 +0900

    feat. 참조&포탈 실습 - 컴포넌트간의 상태공유

--- 08-Refs-Portals/src/components/ResultModal.jsx
+++ 08-Refs-Portals/src/components/ResultModal.jsx
@@ -1,31 +1,38 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
 
   // 이 컴포넌트 외부에서 프로퍼티와 메서드를 통해 접근할 수 있도록 해주는 리액트 훅
   // 첫 번째 인수: ref 객체
   // 두 번째 인수: 함수
-  useImperativeHandle;
-  ref,
-    () => {
-      return {
-        open() {
-          dialog.current.showModal();
-        },
-      };
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
     };
+  });
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemaingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
