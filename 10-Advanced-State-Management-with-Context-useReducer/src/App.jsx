117198a286e68b3d35739c7f24d4a42ab63c10c4
MINJI <always0524@naver.com>
2025-09-11 04:37:41 +0900

    refactor. useReducer - useReducer 훅 사용

--- 10-Advanced-State-Management-with-Context-useReducer/src/App.jsx
+++ 10-Advanced-State-Management-with-Context-useReducer/src/App.jsx
@@ -2,13 +2,13 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import Product from "./components/Product.jsx";
-import { CartContextProvider } from "./store/shopping-cart-context.jsx";
+import CartContextProvider from "./store/shopping-cart-context.jsx";
 
 function App() {
   return (
     <CartContextProvider>
       <Header />
-      <Shop onAddItemToCart={handleAddItemToCart}>
+      <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
             <Product {...product} />
