d0a6142acee2126c60515b01eed2f12fa51f724c
MINJI <always0524@naver.com>
2025-09-10 16:30:28 +0900

    feat. 컨텍스트 API - 컨텍스트 소비

--- 10-Advanced-State-Management-with-Context-useReducer/src/components/Cart.jsx
+++ 10-Advanced-State-Management-with-Context-useReducer/src/components/Cart.jsx
@@ -1,5 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const cartCtx = useContext(CartContext); // useContext는 컨테스트 컴포넌트를 받음
+
+  const totalPrice = cartCtx.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
@@ -7,10 +12,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
