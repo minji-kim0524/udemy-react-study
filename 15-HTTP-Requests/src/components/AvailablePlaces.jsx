a9c95b7cdcaa66b47bd8a0bc58a7c32c48586a2e
MINJI <always0524@naver.com>
2025-10-06 17:51:10 +0900

    learn. HTTP 요청보내기 - fetch data 변환

--- 15-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 import Error from "./Error.jsx";
+import sortPlacesByDistance from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -20,15 +21,22 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later.",
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
     // .then((response) => {
     //   return response.json();
