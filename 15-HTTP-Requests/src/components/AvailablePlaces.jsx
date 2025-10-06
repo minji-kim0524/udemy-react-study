8e86c837f68b8caa72fe7ac52645d1ec4f5ad937
MINJI <always0524@naver.com>
2025-10-06 17:12:15 +0900

    learn. HTTP 요청보내기 - 로딩상태(state) 관리하기

--- 15-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     // .then((response) => {
     //   return response.json();
@@ -23,6 +26,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
